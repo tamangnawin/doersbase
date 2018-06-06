e9ec85a309cbe86fa4d4cda045ecc422e7a26590
Nawin Tamang <37071638+tamangnawin@users.noreply.github.com>
2018-06-06 22:25:10 +0545

    doersBase commit

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $(".nav-bars").click(function(){
+        $(".navigation-sidebar").animate({left: '0px'});
+    });
+    $(".fa-window-close").click(function(){
+        $(".navigation-sidebar").animate({left: '-100%'});
+    });
+});
+
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    
+    // Enable Carousel Controls
+    $(".carousel-control-prev").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".carousel-control-next").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+
+var marginY = 0;
+var destination = 0;
+var speed = 15;
+var scroller = null;
+
+function pageScroll(elementId) {
+    destination = document.getElementById(elementId).offsetTop;
+    
+    scroller = setTimeout(function(){
+        pageScroll(elementId);
+    }, 1);
+    
+    marginY = marginY + speed;
+    
+    if(marginY >= destination) {
+        clearTimeout(scroller);
+    }
+    window.scroll(0, marginY);
+}
+
+function topScroll() {   
+    scroller = setTimeout(function(){
+        topScroll();
+    }, 1);
+    
+    marginY = marginY - speed;
+    
+    if(marginY <= 0) {
+        clearTimeout(scroller);
+    }
+    window.scroll(0, marginY);
+}
+
+

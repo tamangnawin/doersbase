bd5f936ee67404e1f8fa2b91a5f6a20b507b6843
Nawin Tamang <37071638+tamangnawin@users.noreply.github.com>
2018-06-12 17:46:41 +0545

    Team Slider Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,3 +125,26 @@ $(currentDayID).toggleClass("today"); //hightlights today in the view hours moda
 setInterval(checkTime, 1000);
 checkTime();
 
+$("#myCarousel").on("slide.bs.carousel", function(e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $(".carousel-item").length;
+  
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item")
+            .eq(i)
+            .appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item")
+            .eq(0)
+            .appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+  
